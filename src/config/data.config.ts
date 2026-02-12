2b5fb3a01a1a03a0765af64367486f0f14f482ab
krfarshad <krfarshad@gmail.com>
2026-02-12 18:11:57 +0330

    chore: initial static data

--- src/config/data.config.ts
+++ src/config/data.config.ts
@@ -1,6 +1,29 @@
-export const taskListData = [
+import { Board } from "@/features/board";
+import { Task } from "@/features/task";
+
+export const seedBoards: Board[] = [
+  { id: "board-1", title: "Todo", position: 65535 },
+  { id: "board-2", title: "In Progress", position: 131070 },
+  { id: "board-3", title: "Done", position: 196605 },
+];
+
+export const seedTasks: Task[] = [
   {
-    name: "Task List",
+    id: "task-1",
+    boardId: "board-1",
+    title: "Create interview Kanban",
+    position: 65535,
+  },
+  {
+    id: "task-2",
+    boardId: "board-1",
+    title: "Review Drag & Drop",
+    position: 131070,
+  },
+  {
+    id: "task-3",
+    boardId: "board-2",
+    title: "Set up Next.js project",
     position: 65535,
   },
 ];
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,1 @@
-export * from "./api.config";
 export * from "./data.config";
-export * from "./queryKeys.config";
-export * from "./variables.config";
